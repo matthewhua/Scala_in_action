53e14e275cdf6f20ca7dec342ced98d7ef8ce2c3
HUAHandSome <handsomematthew@163.com>
2021-01-04 13:27:52 +0800

    这是第一次scala 学习视频上传

---
+++ src/handsome/matt/foundation/Scala01_HelloWorld.scala
@@ -0,0 +1,17 @@
+package handsome.matt.foundation
+
+/**
+ * @author Matthew
+ * @date 2020-12-30 22:38
+ */
+object Scala01_HelloWorld {
+
+ /*
+ >Unit 相当于java的void
+ * */
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala")
+
+  }
+
+}
